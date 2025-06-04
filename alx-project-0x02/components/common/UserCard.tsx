a9af5422d9ede6d600d2820bd76947a216aec063
Besomi22 <sbelllali000@gmail.com>
2025-06-04 14:32:46 +0100

    Build a UserCard Component

--- alx-project-0x02/components/common/UserCard.tsx
+++ alx-project-0x02/components/common/UserCard.tsx
@@ -3,10 +3,10 @@ import { type UserProps } from "@/interfaces";
 
 const UserCard: React.FC<UserProps> = ({ name, email, address }) => {
     return (
-        <div className="border rounded-lg p-4 shadow mb-4">
+        <div className="border text-white rounded-lg p-4 shadow mb-4">
             <h2 className="text-xl font-semibold">{name}</h2>
-            <p className="text-gray-600">{email}</p>
-            <p className="text-sm text-gray-500">
+            <p className="text-gray-400">{email}</p>
+            <p className="text-sm text-gray-400">
                 {address.street}, {address.suite}, {address.city}, {address.zipcode}
             </p>
         </div>
